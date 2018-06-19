c8f762da730ce60743d713a165301d528baf8d72
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-06-19 11:16:14 +0200

    Add benchmarks

---
+++ plist-cil.benchmark/BinaryPropertyListParserBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using System.IO;
+
+namespace Claunia.PropertyList.Benchmark
+{
+    [CoreJob]
+    [MemoryDiagnoser]
+    public class BinaryPropertyListParserBenchmarks
+    {
+        private byte[] data = null;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = File.ReadAllBytes("plist.bin");
+        }
+
+        [Benchmark]
+        public NSObject ReadLargePropertylistTest()
+        {
+            var nsObject = PropertyListParser.Parse(this.data);
+            return nsObject;
+        }
+    }
+}
